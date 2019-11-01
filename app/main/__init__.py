af0d704f06d142d91267d9c89e12573e166fb1d2
starsandlzh <starsandlzh@gmail.com>
2019-11-01 11:21:23 +0800

    更新项目结构

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,7 +6,7 @@
 from flask import Blueprint
 from flask import jsonify
 from flask_restful import Api
-from app.main.resources.hello import Hello
+from app.main.routes.hello import Hello
 
 blueprint = Blueprint('main', __name__)
 api = Api(blueprint)
