620b90038087eb6a8bc2c6230be6f95aa4cbee47
邵鲁玉 <516508236@qq.com>
2019-11-13 10:46:36 +0800

    删除一些测试代码文件

--- app/task/celery_app.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Description: 创建Celery应用
-# Created: shaoluyu 2019/10/25
-# Modified: shaoluyu 2019/10/25;
-
-from app.task.make_celery import make_celery
-
-# 创建celery应用
-celery = make_celery(flask_app=None)
