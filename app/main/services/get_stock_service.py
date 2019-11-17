29292492d6d9cc4696ab3e8977eb1eb476fbd545
邵鲁玉 <516508236@qq.com>
2019-11-17 09:27:57 +0800

    lock

--- app/main/services/get_stock_service.py
+++ app/main/services/get_stock_service.py
@@ -7,6 +7,7 @@ import redis
 from flask import current_app
 
 from app.main.redis_pool import redis_pool
+from app.utils.reids_lock import RedisLock
 
 
 def get_stock():
@@ -15,15 +16,21 @@ def get_stock():
     :return:
     """
     redis_conn = redis.Redis(connection_pool=redis_pool)
-    # 获取Redis库存数据
-    json_stock_list = redis_conn.get('gc:stocks')
-    # 如果数据存在
-    if json_stock_list:
-        current_app.logger.info('get stock_list from redis')
-        result_list = json.loads(json_stock_list)
-        return result_list
-        # 如果数据过期或被删除
+    lock_id = RedisLock.try_lock(redis_conn, 'stock_lock', wait_time=20)
+    # 拿到锁，获取库存
+    if lock_id:
+        # 获取Redis库存数据
+        json_stock_list = redis_conn.get('gc:stocks')
+        # 如果数据存在
+        if json_stock_list:
+            current_app.logger.info('get stock_list from redis')
+            result_list = json.loads(json_stock_list)
+            return result_list
+            # 如果数据过期或被删除
+        else:
+            raise RuntimeError('redis data error')
     else:
         raise RuntimeError('redis data error')
 
+    RedisLock.unlock(redis_conn, 'stock_lock', lock_id)
     redis_conn.close()
