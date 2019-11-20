6165d904080a965344240243a50bc26981813eb2
starsandlzh <starsandlzh@gmail.com>
2019-11-20 13:42:11 +0800

    更新发货单相关实体类

--- app/main/entity/delivery_sheet.py
+++ app/main/entity/delivery_sheet.py
@@ -21,16 +21,6 @@ class DeliverySheet(BaseEntity):
         self.weight = None  # 理重
         self.create_time = None  # 创建时间
         self.update_time = None  # 更新时间
-
         if delivery_sheet:
-            self.rid = ""  # 主键id
-            self.delivery_no = delivery_sheet['delivery_sheet']  # 发货通知单号
-            self.batch_no = delivery_sheet['batch_no']  # 批次号
-            self.data_address = delivery_sheet["data_address"]  # 数据来源
-            self.items = [DeliveryItem(i) for i in delivery_sheet["items"]]  # 发货通知单子单
-            self.total_quantity = delivery_sheet["total_quantity"]  # 总件数
-            self.free_pcs = delivery_sheet["free_pcs"]  # 散根数
-            self.total_pcs = delivery_sheet["total_pcs"]  # 总根数
-            self.weight = delivery_sheet["weight"]  # 理重
-            self.create_time = ""  # 创建时间
-            self.update_time = ""  # 更新时间
+            self.set_attr(delivery_sheet)
+
