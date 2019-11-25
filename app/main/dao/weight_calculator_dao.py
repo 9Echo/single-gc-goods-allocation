305a0ab2b9c1fa503293db28484bedfaefe96fc4
毕钰爽 <biyushuang@163.com>
2019-11-25 18:24:04 +0800

    1

--- app/main/dao/weight_calculator_dao.py
+++ app/main/dao/weight_calculator_dao.py
@@ -10,15 +10,16 @@ from app.main.db_pool import db_pool_trans_plan, db_pool_ods
 
 class WeightCalculatorDao(BaseDao):
 
-    def get_data_from_table(self):
+    def get_data_from_table(self, test_list):
         '''
         获取t_calculator_item数据
         # 更新t_calculator_item时，sql加where CNAME = '方矩管' or CNAME = '热镀方矩管'
         :return:
         '''
-        sql = """select * from t_calculator_item """
-
-        data = self.select_all(sql)
+        sql = """select * from t_calculator_item 
+                where CNAME = %s and ITEMID = %s"""
+        values = [test_list[0], test_list[1]]
+        data = self.select_all(sql, values)
         return data
 
     def update_data(self, update_list):
