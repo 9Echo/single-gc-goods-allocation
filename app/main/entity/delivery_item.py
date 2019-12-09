5d2381ec3a93a8092c3c3c4284af63c58fae01e2
承涛 <chengtt0719@sina.com>
2019-12-09 13:54:10 +0800

    修改

--- app/main/entity/delivery_item.py
+++ app/main/entity/delivery_item.py
@@ -8,22 +8,99 @@ class DeliveryItem(BaseEntity):
     """发货通知单子项"""
 
     def __init__(self, delivery_item=None):
+        """
+        暂时都把字段添加了,注释在上面的是新添加的，有几个含义相同单名字不同的已经注释了，可参考修改
+        """
         self.rid = None  # 主键id
         self.delivery_no = None  # 发货通知单主单号
         self.delivery_item_no = None  # 子单号
-        self.order_no = None # 订单号
+        self.order_no = None  # 订单号
         self.product_type = None  # 产品类型
-        self.item_id = None
+        self.item_id = None   # 物资代码
         self.spec = None  # 产品规格
-        self.ghoo = None  # 材质
+        self.gh00 = None  # 材质
         self.weight = None  # 产品重量
         self.weightone = None  # 根重
         self.warehouse = None  # 仓库信息
         self.loc_id = None  # 库位信息
+        # # 仓库 warehouse
+        # self.f_hws = None
+        # # 垛号 loc_id
+        # self.f_loc = None
         self.quantity = None  # 总数
         self.free_pcs = None  # 散根数
         self.total_pcs = None  # 总根数
+        # # 件数
+        # self.order_j = None
+        # # 散根数
+        # self.order_g = None
+        # # 总根数
+        # self.order_zg00 = None
         self.create_time = None  # 创建时间
         self.update_time = None  # 更新时间
+        # 创建时间 create_time
+        self.crted_date = None
+        # 更新时间 等于create_time
+        self.upted_date = None
+        # HL
+        self.corpid = None
+        # 出入库标记
+        self.i_o = None
+        # 录入人ID-前端传入
+        self.doctype = None
+        # 提货单号-前端传入
+        self.docuno = None
+        # 序号1,2,3
+        self.lno = None
+        # 客户公司简称-前端传入
+        self.org_unit = None
+        # CRTED_DATE 取年月日
+        self.trx_date = None
+        # n
+        self.lot_no = None
+        # 单位kg
+        self.uom_id = None
+        # 录入人首字母-前端传入
+        self.lrr = None
+        # 销售部门-前端传入
+        self.salesorg = None
+        # 销售员-前端传入
+        self.slsman = None
+        # LRR
+        self.upted_by = None
+        # 物资代码大类 ITEMA.CLASSID(CATEGORIES.CATEGORY_ID)
+        self.catno = None
+        # ??数字或字母
+        self.subcls = None
+        # 备注
+        self.bz00 = None
+        # N
+        self.dibz = None
+        # t 单位 吨
+        self.order_dw = None
+        # 1000.00 物料单位转换
+        self.uomrate = None
+        # 1
+        self.validzl = None
+        # n
+        self.quality = None
+        # n
+        self.maker = None
+        # n
+        self.rkrq = None
+        # n
+        self.zllevel = None
+        # n
+        self.packagespec = None
+        # n
+        self.htno = None
+        # n
+        self.custname = None
+        # 空值
+        self.dgwidth = None
+        # 空值
+        self.dgheight = None
+        # 空值
+        self.dglength = None
         if delivery_item:
             self.set_attr(delivery_item)
--- app/main/entity/delivery_sheet.py
+++ app/main/entity/delivery_sheet.py
@@ -8,6 +8,9 @@ class DeliverySheet(BaseEntity):
     """发货通知单"""
 
     def __init__(self, delivery_sheet=None):
+        """
+            暂时都把字段添加了,注释在上面的是新添加的，有几个含义相同单名字不同的已经注释了，可参考修改
+        """
         self.rid = None  # 主键id
         self.delivery_no = None  # 发货通知单号
         self.load_task_id = None  # 所属车次号
@@ -18,15 +21,71 @@ class DeliverySheet(BaseEntity):
         self.total_quantity = None  # 总件数
         self.free_pcs = None  # 散根数
         self.total_pcs = None  # 总根数
+        # # 总件数
+        # self.order_j = None
+        # # 散根数
+        # self.order_g = None
+        # # 总根数
+        # self.order_zg00 = None
         self.weight = None  # 理重
+        # # 理重
+        # self.order_cal = None
         self.order_no = None  # 订单id
         self.doc_type = None  # 开单员id
+        # # 录入人ID-前端传入
+        # self.doctype = None
         self.lrr = None  # 开单员
+        # # 录入人即开单员名字首字母-前端传入
+        # self.lrr = None
         self.customer_id = None  # 客户id
         self.salesorg = None  # 销售部门id
         self.salesman_id = None  # 销售员id
+        # # 销售部门ID-前端传入
+        # self.salesorg = None
+        # # 销售员ID-前端传入
+        # self.slsman = None
         self.create_time = None  # 创建时间
         self.update_time = None  # 更新时间
+        # # 创建时间
+        # self.crted_date = None
+        # # 更新时间
+        # self.upted_date = None
+        # HL
+        self.corpid = None
+        # 出入库标记
+        self.i_o = None
+        # 确认发货通知单后查询开单序号表分配单号
+        self.docuno = None
+        # 客户公司简称-直接取值，开单时通过请求信息拿到
+        self.org_unit = None
+        # CRTED_DATE 的年月日
+        self.trx_date = None
+        # 0.00
+        self.trx_qty = None
+        # 0.00
+        self.trx_amt = None
+        # 0
+        self.trx_zg00 = None
+        # LRR
+        self.upted_by = None
+        # 仓库
+        self.f_whs = None
+        # 垛号
+        self.f_loc = None
+        # 0
+        self.trx_j = None
+        # 0
+        self.trx_g = None
+        # 估重合计
+        self.order_twe = None
+        # 贷款合计
+        self.order_amt = None
+        # 0.00 运费合计
+        self.fee_order = None
+        # 空着
+        self.note = None
+        # N
+        self.dibz = None
         if delivery_sheet:
             self.set_attr(delivery_sheet)
 
