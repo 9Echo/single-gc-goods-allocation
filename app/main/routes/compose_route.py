fe31bcc1a9313265a88ee447fbef7aea38c14a22
邵鲁玉 <516508236@qq.com>
2019-12-12 16:28:24 +0800

    1

--- app/main/routes/compose_route.py
+++ app/main/routes/compose_route.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2019/11/15 16:17
 # @Author  : Zihao.Liu
-from app.main.entity.delivery_item import DeliveryItem
-from app.main.entity.delivery_sheet import DeliverySheet
-from app.main.entity.order import Order
-from app.utils.uuid_util import UUIDUtil
+from flask import request
+from flask_restful import Resource
 
 
-def generate_delivery_sheet(order):
-    ds = DeliverySheet()
-    ds.delivery_no = UUIDUtil.create_id("delivery")
-    ds.free_pcs = 0
-    ds.total_quantity = 0
-    ds.items = []
-    for orderitem in order.order_item:
-        di = DeliveryItem()
-        di.quantity = orderitem.quantity
-        ds.total_quantity += di.quantity
-        di.free_pcs = orderitem.free_pcs
-        ds.free_pcs += di.free_pcs
-        di.product_type = orderitem.product_type
-        ds.items.append(di)
-    return ds
+class ComposeRoute(Resource):
+    """
+    拼单推荐接口
+    """
+    @staticmethod
+    def post(order_no):
+        """进行拼货推荐"""
+        # 获取输入参数（发货通知单）
+        delivery_data = request.get_json(force=True).get('data')  # 入参是json
 
 
-def compose(order_no):
-    """进行订单分货"""
-    order = Order()
-    order.order_item = order_item_left_dao.get_all()
-    # stocks = get_stock()
-    return generate_delivery_sheet(order)
+
+
+    # def generate_delivery_sheet(order):
+    #     ds = DeliverySheet()
+    #     ds.delivery_no = UUIDUtil.create_id("delivery")
+    #     ds.free_pcs = 0
+    #     ds.total_quantity = 0
+    #     ds.items = []
+    #     for orderitem in order.order_item:
+    #         di = DeliveryItem()
+    #         di.quantity = orderitem.quantity
+    #         ds.total_quantity += di.quantity
+    #         di.free_pcs = orderitem.free_pcs
+    #         ds.free_pcs += di.free_pcs
+    #         di.product_type = orderitem.product_type
+    #         ds.items.append(di)
+    #     return ds
