ace9b0b5e3067732f4c5db34d7cb522aa6f9b13f
邵鲁玉 <516508236@qq.com>
2019-12-18 17:23:22 +0800

    1

--- app/main/routes/order_route.py
+++ app/main/routes/order_route.py
@@ -4,10 +4,11 @@
 # Modified: shaoluyu 2019/11/13
 import json
 
-from flask import request
+from flask import request, current_app
 from flask_restful import Resource
 
 from app.main.services import order_service, dispatch_service
+from app.utils.my_exception import MyException
 from app.utils.result import Result
 
 
@@ -19,9 +20,18 @@ class OrderRoute(Resource):
     def post(self):
         """输入订单，返回开单结果
         """
-        json_data = json.loads(request.get_data().decode("utf-8"))
-        order = order_service.generate_order(json_data['data'])
-        sheets = dispatch_service.dispatch(order)
-        return Result.success_response(sheets)
+        try:
+            json_data = json.loads(request.get_data().decode("utf-8"))
+            order = order_service.generate_order(json_data['data'])
+            sheets = dispatch_service.dispatch(order)
+            return Result.success_response(sheets)
+        except MyException as me:
+            current_app.logger.info(me.message)
+            current_app.logger.exception(me)
+            return Result.error_response(me.message)
+        except Exception as e:
+            current_app.logger.info("order error")
+            current_app.logger.exception(e)
+            return Result.error_response()
 
 
