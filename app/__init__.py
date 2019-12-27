f0217f6033c3fff1504c4bcf85197823f967eae9
邵鲁玉 <516508236@qq.com>
2019-12-27 19:00:09 +0800

    1

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ def _access_control(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = (
         'GET,HEAD,PUT,PATCH,POST,DELETE')
-    # response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     response.headers['Access-Control-Max-Age'] = 86400
     return response
 
