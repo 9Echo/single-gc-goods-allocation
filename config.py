04d17c3a945ec2066ef3f43babe3a37bc75dce34
邵鲁玉 <516508236@qq.com>
2020-03-16 15:39:01 +0800

    修改

--- config.py
+++ config.py
@@ -142,8 +142,8 @@ class TestConfig(Config):
 
     # Redis配置，可选（不使用时可删除）
     REDIS_HOST = '47.99.118.183'
-    REDIS_PORT = '6379'
-    REDIS_PASSWD = 'wobugaoxing'
+    REDIS_PORT = '6389'
+    REDIS_PASSWD = 'JCdev@56zh'
     REDIS_MAX_CONNECTIONS = 2
 
     # APScheduler定时任务配置，可选（不使用时可删除）
