b8a8051e9b9cf9791e64f8b9f3703e286d0367cd
周文涛 <898783672@qq.com>
2020-03-30 17:38:28 +0800

    返回参数格式调整

--- app/main/routes/order_route.py
+++ app/main/routes/order_route.py
@@ -30,5 +30,5 @@ class OrderRoute(Resource):
             # 综合
             sheets_3 = dispatch_service_optimize.dispatch(order)
 
-            return Result.success_response(sheets_1+sheets_2+sheets_3)
+            return Result.success_response(sheets_1)
 
