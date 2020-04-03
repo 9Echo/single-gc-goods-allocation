e3205f1920040d142f2bed67bbbe55e938064f42
邵鲁玉 <516508236@qq.com>
2020-04-03 09:43:10 +0800

    1

--- app/utils/aspect/aspect.py
+++ app/utils/aspect/aspect.py
@@ -9,16 +9,20 @@ from flask import current_app
 
 @blueprint.before_request
 def before_request():
+    current_app.logger.info('===========================start=============================')
     current_app.logger.info('before_request')
     current_app.logger.info('method is ' + request.method)
     current_app.logger.info('url is ' + request.url)
     current_app.logger.info('body is ' + str(request.json).replace('\'', '"'))
     current_app.logger.info('ip is ' + request.remote_addr)
+    current_app.logger.info('===========================end=============================')
 
 
 @blueprint.after_request
 def after_request(response):
+    current_app.logger.info('===========================start=============================')
     current_app.logger.info("after_request")
     current_app.logger.info("body is " + str(response.json).replace("\'", '"'))
     current_app.logger.info("time is " + str(datetime.now())[:19])
+    current_app.logger.info('===========================end=============================')
     return response
