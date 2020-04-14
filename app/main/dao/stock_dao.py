2b4e4d124a66b70852f96f79c4dd25455014ed38
承涛 <chengtt0719@sina.com>
2020-04-14 11:03:30 +0800

    '修改查询时间'

--- app/main/dao/stock_dao.py
+++ app/main/dao/stock_dao.py
@@ -1,5 +1,7 @@
 from app.main.db_pool import db_pool_ods
 import pymysql
+from datetime import datetime
+import time
 
 
 def select_stock():
@@ -14,6 +16,7 @@ def select_stock():
     """
     conn = db_pool_ods.connection()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
+    date = time.strftime("%Y-%m-%d", time.localtime(time.mktime(datetime.timetuple(datetime.now())) - 86400))
     sql = """
             SELECT
                 t2.prod_kind_price_out as prod_kind_price_out,
@@ -35,15 +38,14 @@ def select_stock():
                         prod_kind 
                 ) t2 
             WHERE
-                record_day = '2020-04-12' 
+                record_day = '{}' 
                 AND PRODCOMPANY NOT IN ( '日照钢铁供应有限公司', '日照京华管业有限公司' ) 
                 AND t1.COMMODITYNAME = t2.prod_kind
                 AND t1.CANSENDNUMBER > 0
                 AND t1.CANSENDWEIGHT is not NULL 
                 AND t1.CITY not in ('青岛市','临沂市','日照市')
         """
-    cursor.execute(sql)
+    cursor.execute(sql.format(date))
     return cursor.fetchall()
-
 
 
