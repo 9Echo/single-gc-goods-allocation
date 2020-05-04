5017278e8b6f3248875106a874924df8c5b2cca8
承涛 <chengtt0719@sina.com>
2020-05-04 15:17:06 +0800

    1

--- app/main/services/steel_dispatch_service.py
+++ app/main/services/steel_dispatch_service.py
@@ -23,14 +23,19 @@ def dispatch() -> List[LoadTask]:
     1 将单条库存数据满足标载条件[31,33]的数据直接生成车次，并搜索满足一装一卸的可拼的其他货物，拼到该车次，其中包含超期或催货标为急发车次，并且类型为一装一卸
     2 按照优先发运、最新挂单时间进行正序排序
     3 选择第一条作为目标数据，将除第一条其余数据中出库仓库和区县与其相同或卸货地址与其相同的数据筛选出来作为待选集
-    4 按照优先级将待选集与目标数据利用背包进行匹配，使重量最大化，匹配成功（总量在[31,33]）生成车次，标注是否急发和类型，匹配不成功放入甩货列表，优先级依次为：一装一卸，两装一卸（同区仓库），两装一卸(不同区仓库),一装两卸
+    4 按照优先级将待选集与目标数据利用背包进行匹配，使重量最大化，匹配成功（总量在[31,33]）生成车次，标注是否急发和类型，匹配不成功放入甩货列表，
+    优先级依次为：一装一卸，两装一卸（同区仓库），两装一卸(不同区仓库),一装两卸
     5 重复3和4，直到结束
     """
+
+    load_task_id = 0
     # 根据车辆条件获取库存
     end_point_stock_list_dict: Dict[str, List[Stock]] = dict()
     result_list: List[LoadTask] = list()
     stock_list: List[Stock] = stock_service.deal_stock()
-    print('今日0点总库存:' + str(sum(i.CANSENDWEIGHT for i in stock_list)) + 'kg')
+    for i in copy.copy(stock_list):
+        if 31000 <= i.CANSENDWEIGHT <= 33000:
+            pass
     for i in stock_list:
         end_point_stock_list_dict.setdefault(i.end_point, []).append(i)
     # 过滤库存，调用算法进行配货
@@ -124,6 +129,29 @@ def split_item(item: Stock, delta_weight: float) -> Tuple[Stock, Stock]:
     item.CANSENDWEIGHT = item.CANSENDWEIGHT - new_weight
     item.CANSENDNUMBER = item.CANSENDNUMBER - new_cunt
     return item, new_item
+
+
+def create_load_task(stock_list: List[Stock], load_task_id) -> List[LoadTask]:
+    total_weight = sum(i.CANSENDWEIGHT for i in stock_list)
+    load_task_id += 1
+    load_task_list = list()
+    for i in stock_list:
+        load_task = LoadTask()
+        load_task.load_task_id = load_task_id
+        load_task.total_weight = total_weight
+        load_task.weight = i.CANSENDWEIGHT
+        load_task.count = i.CANSENDNUMBER
+        load_task.city = i.CITY
+        load_task.end_point = i.REGIONS
+        load_task.commodity = i.COMMODITYNAME
+        load_task.notice_num = i.NOTICENUM
+        load_task.oritem_num = i.ORITEMNUM
+        load_task.standard = i.STANDARD
+        load_task.sgsign = i.MATERIAL
+        load_task.outstock_code = i.DELIWAREHOUSE
+        load_task.instock_code = i.DELIWARE
+        load_task_list.append(load_task)
+    return load_task_list
 
 
 if __name__ == '__main__':
