6969135bfdded0bcfbe44a3a2e966c50bde174d6
邵鲁玉 <516508236@qq.com>
2020-05-04 13:16:12 +0800

    车次类

--- app/main/entity/load_task.py
+++ app/main/entity/load_task.py
@@ -9,7 +9,31 @@ class LoadTask(BaseEntity):
 
     def __init__(self):
         self.load_task_id = None  # 所属车次号
-        self.items = []  # 明细
+        # 优先级
+        self.priority = None
+        # 装卸类型
+        self.type = None
+        # 总重量
+        self.total_weight = 0
+        # 发货通知单重量
         self.weight = 0
+        # 发货通知单件数
+        self.count = 0
+        # 城市
         self.city = None
+        # 区县
         self.end_point = None
+        # 品种
+        self.commodity = None
+        # 发货通知单号
+        self.notice_num = None
+        # 订单项次号
+        self.oritem_num = None
+        # 规格
+        self.standard = None
+        # 材质
+        self.sgsign = None
+        # 出库仓库
+        self.outstock_code = None
+        # 入库仓库
+        self.instock_code = None
