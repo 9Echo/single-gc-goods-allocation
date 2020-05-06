9e2bb0bf65d46919ad43d866275c62b10d76bee9
邵鲁玉 <516508236@qq.com>
2020-05-06 09:59:06 +0800

    static

--- app/main/services/stock_service.py
+++ app/main/services/stock_service.py
@@ -4,8 +4,12 @@
 
 
 import copy
+import os
+
 from app.main.entity.stock import Stock
 import pandas as pd
+
+from app.utils.get_static_path import get_path
 
 
 def get_stock():
@@ -27,8 +31,9 @@ def get_stock():
     #     stock = Stock(data)
     #     stock_list.append(stock)
     # return stock_list
-    df_stock1 = pd.read_excel("sheet1.xls", 1)
-    df_stock3 = pd.read_excel("sheet1.xls", 4)
+    data_path = get_path('sheet1.xls')
+    df_stock1 = pd.read_excel(data_path, 1)
+    df_stock3 = pd.read_excel(data_path, 4)
     return pd.concat([df_stock1, df_stock3], ignore_index=True)
 
 
@@ -62,7 +67,8 @@ def deal_stock():
     df_stock["件重"] = df_stock["实际可发重量"] / df_stock["实际可发件数"]
     # 根据短溢的重量，扣除相应的实际可发件数和实际可发重量,此处math.ceil向上取出会报错，所以用的是另一种向上取整方法
     df_stock.loc[df_stock["需短溢重量"] > 0, ["实际可发件数"]] = df_stock["实际可发件数"] + (-df_stock["需短溢重量"] // df_stock["件重"])
-    df_stock.loc[df_stock["需短溢重量"] > 0, ["实际可发重量"]] = df_stock["实际可发重量"] + df_stock["件重"] * (-df_stock["需短溢重量"] // df_stock["件重"])
+    df_stock.loc[df_stock["需短溢重量"] > 0, ["实际可发重量"]] = df_stock["实际可发重量"] + df_stock["件重"] * (
+                -df_stock["需短溢重量"] // df_stock["件重"])
     # 区分西老区的开平板
     df_stock.loc[(df_stock["品名"] == "开平板") & (df_stock["出库仓库"].str.startswith("P")), ["品名"]] = ["西区开平板"]
     df_stock.loc[(df_stock["品名"] == "开平板") & (df_stock["出库仓库"].str.startswith("P") == False), ["品名"]] = ["老区开平板"]
