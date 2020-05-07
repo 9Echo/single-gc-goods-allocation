96b184d4b17a80d093bef93a122ad79d06084af6
周文涛 <898783672@qq.com>
2020-05-07 16:37:03 +0800

    1

--- app/main/services/generate_excel_service.py
+++ app/main/services/generate_excel_service.py
@@ -6,10 +6,10 @@ from app.main.services.steel_dispatch_service import dispatch
 
 def generate_excel(load_task_list: List[LoadTask]):
     df = pd.DataFrame([item.as_dict() for item in load_task_list])
+    writer = pd.ExcelWriter("库存.xlsx")
+    df.to_excel(writer,sheet_name="分货车次明细")
 
-    # df.to_excel("sheet3.xls")
-
-    df1 = df.groupby(['city', 'end_point', 'commodity']).agg(
+    df1 = df.groupby(['city', 'end_point', 'big_commodity']).agg(
         {'weight': 'sum'}).reset_index()
     df2 = df.groupby(['city', 'end_point'])['load_task_id'].nunique().reset_index()
     load_list=[]
@@ -20,15 +20,18 @@ def generate_excel(load_task_list: List[LoadTask]):
     # 增加新的一列车次数
     df1['load_num'] = load_list
 
-    df1.to_excel("sheet3.xls")
-    # df1.rename(index=str,
-    #                  columns={
-    #                      "city": "城市",
-    #                      "end_point": "区县",
-    #                      "sum": "总重量",
-    #                      "count": "车次数"
-    #                  },
-    #                  inplace=True)
+    df1.rename(index=str,
+                     columns={
+                         "city": "城市",
+                         "end_point": "区县",
+                         "big_commodity":"大品种",
+                         "weight": "总重量",
+                         "load_num": "车次数"
+                     },
+                     inplace=True)
+
+    df1.to_excel(writer,sheet_name="分货车次汇总")
+    writer.save()
     print(df1)
 
 
