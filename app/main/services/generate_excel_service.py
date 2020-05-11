a065b792539d479ddaa337868e87300f083f101f
周文涛 <898783672@qq.com>
2020-05-11 16:06:09 +0800

    修改混装配载规则

--- app/main/services/generate_excel_service.py
+++ app/main/services/generate_excel_service.py
@@ -2,7 +2,7 @@ import pandas as pd
 from typing import List
 from app.main.entity.load_task import LoadTask
 from app.utils.get_static_path import get_path
-
+import operator
 
 def generate_excel(load_task_list: List[LoadTask]):
     df = pd.DataFrame([item.as_dict() for item in load_task_list])
@@ -14,30 +14,47 @@ def generate_excel(load_task_list: List[LoadTask]):
         {'weight': 'sum'}).reset_index()
     df2 = df.groupby(['city', 'end_point'])['load_task_id'].nunique().reset_index()
     load_list = []
-    joint_list = []
+    load_advice_list = []
     # 统计区县配载建议以及车次数
     for index, row in df1.iterrows():
+        joint_str_list = []
         temp_df_num = df2[(df2['city'] == row['city']) & (df2['end_point'] == row['end_point'])]
         load_list.append(list(temp_df_num['load_task_id'])[0])
         temp_df=df[(df['city'] == row['city']) & (df['end_point'] == row['end_point'])]
-        for i, v in temp_df['load_task_id'].iteritems():
-            joint_str=None
-            unique_big_commodity=temp_df[temp_df['load_task_id']==v]['big_commodity'].unique().tolist()
-            if len(unique_big_commodity)!=1:
-                joint_str=unique_big_commodity[0]
-                for joint_index in range(1,len(unique_big_commodity)):
-                    joint_str=joint_str+"和"+unique_big_commodity[joint_index]
-                joint_str=joint_str+"混装,"
-        joint_list.append(joint_str)
+        for load_id in temp_df['load_task_id'].unique().tolist():
+            unique_big_commodity=temp_df[temp_df['load_task_id']==load_id]['big_commodity'].unique().tolist()
+            if len(unique_big_commodity)>1:
+                joint_str_list.append(unique_big_commodity)
+        # 去除重复的配载情况
+        news_str_list=[]
+        if len(joint_str_list)!=0:
+            news_str_list.append(joint_str_list[0])
+            for str in joint_str_list:
+                flag=False
+                for new_str_item in news_str_list:
+                    if operator.eq(str,new_str_item):
+                        flag=True
+                if flag==False:
+                    news_str_list.append(str)
+        # 拼接混载字符
+        joint_str=""
+        if len(news_str_list)!=0:
+            for joint_item in news_str_list:
+                temp_str=joint_item[0]
+                for index in range(1,len(joint_item)):
+                    temp_str=temp_str+"和"+joint_item[index]
+                joint_str=joint_str+temp_str+"混装,"
+        load_advice_list.append(joint_str)
+
+
     # 增加新的一列车次数
     df1['load_num'] = load_list
-    df1['load_advice'] = joint_list
+    df1['load_advice'] = load_advice_list
 
 
 
 
-    df.rename(index=str,
-               columns={
+    df.rename( columns={
                    "load_task_id": "车次号",
                    "priority": "优先级",
                    "load_task_type": "装卸类型",
@@ -56,12 +73,12 @@ def generate_excel(load_task_list: List[LoadTask]):
                    "instock_code": "入库仓库",
                    "receive_address": "卸货地址",
                    "price_per_ton": "吨公里/价格",
-                   "remark": "备注(配件)"
+                   "total_price": "总价格",
+                   "remark": "备注(配件)",
                },
                inplace=True)
 
-    df1.rename(index=str,
-               columns={
+    df1.rename(columns={
                    "city": "城市",
                    "end_point": "区县",
                    "big_commodity": "大品种",
