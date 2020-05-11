e721d02cb3b1f4c232be3c28934b229263dd8bb7
周文涛 <898783672@qq.com>
2020-05-11 16:19:12 +0800

    1

--- app/main/services/generate_excel_service.py
+++ app/main/services/generate_excel_service.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from typing import List
+from typing import List,Tuple,Dict
 from app.main.entity.load_task import LoadTask
 from app.utils.get_static_path import get_path
 import operator
@@ -26,26 +26,10 @@ def generate_excel(load_task_list: List[LoadTask]):
             if len(unique_big_commodity)>1:
                 joint_str_list.append(unique_big_commodity)
         # 去除重复的配载情况
-        news_str_list=[]
-        if len(joint_str_list)!=0:
-            news_str_list.append(joint_str_list[0])
-            for str in joint_str_list:
-                flag=False
-                for new_str_item in news_str_list:
-                    if operator.eq(str,new_str_item):
-                        flag=True
-                if flag==False:
-                    news_str_list.append(str)
+        news_str_list=drop_duplicate_list(joint_str_list)
         # 拼接混载字符
-        joint_str=""
-        if len(news_str_list)!=0:
-            for joint_item in news_str_list:
-                temp_str=joint_item[0]
-                for index in range(1,len(joint_item)):
-                    temp_str=temp_str+"和"+joint_item[index]
-                joint_str=joint_str+temp_str+"混装,"
+        joint_str=concat_string(news_str_list)
         load_advice_list.append(joint_str)
-
 
     # 增加新的一列车次数
     df1['load_num'] = load_list
@@ -91,6 +75,40 @@ def generate_excel(load_task_list: List[LoadTask]):
     df1.to_excel(writer, sheet_name="分货车次汇总")
     writer.save()
 
+
+def drop_duplicate_list(joint_str_list :List):
+    """
+      二位数组内部去除重复数组
+      :param joint_str_list:
+      :return:news_str_list
+      """
+    news_str_list = []
+    if len(joint_str_list) != 0:
+        news_str_list.append(joint_str_list[0])
+        for str in joint_str_list:
+            flag = False
+            for new_str_item in news_str_list:
+                if operator.eq(str, new_str_item):
+                    flag = True
+            if flag == False:
+                news_str_list.append(str)
+    return  news_str_list
+
+
+def concat_string(news_str_list):
+    """
+      拼接数组内品种字符
+      :param news_str_list:
+      :return:joint_str
+      """
+    joint_str = ""
+    if len(news_str_list) != 0:
+        for joint_item in news_str_list:
+            temp_str = joint_item[0]
+            for index in range(1, len(joint_item)):
+                temp_str = temp_str + "和" + joint_item[index]
+            joint_str = joint_str + temp_str + "混装,"
+    return  joint_str
 
 if __name__ == '__main__':
     generate_excel()
