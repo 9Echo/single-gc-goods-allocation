5e502fa336b9db56798f497a6fec84fff185c252
邵鲁玉 <516508236@qq.com>
2020-06-06 20:51:55 +0800

    1

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ class DevelopmentConfig(Config):
     REDIS_MAX_CONNECTIONS = 2
 
     # 微服务url
-    DISPATCH_SERVICE_URL = 'http://192.168.1.20:9078'
+    DISPATCH_SERVICE_URL = 'http://192.168.1.70:9078'
 
     # APScheduler定时任务配置，可选（不使用时可删除）
     SCHEDULER_OPEN = False
@@ -150,7 +150,7 @@ class TestConfig(Config):
     REDIS_PASSWD = 'JCdev@56zh'
     REDIS_MAX_CONNECTIONS = 2
     # 微服务url
-    DISPATCH_SERVICE_URL = 'http://192.168.1.20:9078'
+    DISPATCH_SERVICE_URL = 'http://192.168.1.70:9078'
     # APScheduler定时任务配置，可选（不使用时可删除）
     SCHEDULER_OPEN = False
     SCHEDULER_API_ENABLED = True
