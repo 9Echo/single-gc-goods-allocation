e1e39438252ae0bc1a407e756313f1df3413ac30
邵鲁玉 <516508236@qq.com>
2020-06-24 11:52:16 +0800

    修改

--- app/main/steel_factory/dao/out_stock_queue_dao.py
+++ app/main/steel_factory/dao/out_stock_queue_dao.py
@@ -11,52 +11,17 @@ class OutStockQueueDao(BaseDao):
         :return:
         """
         sql = """
-        SELECT
-        stock_code
-        FROM
+        select
+        stock_name
+        from
         (
         SELECT
-        q.*,
-        d.trans_company_id,
-        d.plan_no,
-        p.plan_status,
-        CASE
-        WHEN q.STATUS = 'product_disp_queue_status_00' 
-        OR q.STATUS = 'product_disp_queue_status_21' 
-        OR q.STATUS = 'product_disp_queue_status_22' THEN
-        '1' 
-        WHEN q.STATUS = 'product_disp_queue_status_30' 
-        OR q.STATUS = 'product_disp_queue_status_37' THEN
-        '2' 
-        WHEN q.STATUS = 'product_disp_queue_status_38' THEN
-        '3' 
-        WHEN q.STATUS = 'product_disp_queue_status_40' THEN
-        '4' 
-        WHEN q.STATUS = 'product_disp_queue_status_50' THEN
-        '5' ELSE q.STATUS 
-        END AS status_type,
-        CASE
-            
-            WHEN q.whether_remind = 1 THEN
-            '是' ELSE '否' 
-        END AS whether_remind_name 
+        CONCAT(stock_code,'-',stock_name) as stock_name,
+        (dis_bill_no_jc_trucks_count+dis_cw_trucks_count+dis_jh_trucks_count+dis_jc_no_sign_trucks_count+dis_in_warehouse_trucks_count) as truck_count
         FROM
-        ods_db_queue_t_product_disp_entry_queue q
-        LEFT JOIN ods_db_queue_t_product_disp_procure_deal d ON q.deal_id = d.deal_id
-        LEFT JOIN ods_db_queue_t_plan p ON d.plan_no = p.plan_no 
-        WHERE
-        1 = 1 
-        AND q.queue_type = 3 
-        AND q.company_id = 'C000002000' 
-        AND p.plan_status = 'DDZT68' 
-        AND q.STATUS REGEXP '^(product_disp_queue_status_3|product_disp_queue_status_4|product_disp_queue_status_5)' 
-        ) t 
-        WHERE
-        1 = 1 
-        AND DATE_SUB( CURDATE( ), INTERVAL 1 DAY ) <= date( QUEUE_START_TIME ) 
-        GROUP BY
-        stock_code 
-        HAVING count(1) > 35
+        db_dev.`dws_dis_warehouse_trucks`
+        ) t
+        where truck_count > 35
         """
         data = self.select_all(sql)
         out_stock_list = list()
--- app/main/steel_factory/service/single_stock_service.py
+++ app/main/steel_factory/service/single_stock_service.py
@@ -31,8 +31,7 @@ def get_stock(truck):
     out_stock_list = out_stock_queue_dao.select_out_stock_queue()
     # 去除等待数较高的出库仓库，暂不往该仓库开单
     if out_stock_list:
-        all_stock_list = [i for i in all_stock_list if
-                          (i.get('deliware_house').split('-')[0]) not in out_stock_list]
+        all_stock_list = [i for i in all_stock_list if i.get('deliware_house') not in out_stock_list]
     # 获取已开装车清单信息、预装车清单信息、最大更新时间、开单推荐但未经过确认
     loading_detail_list = loading_detail_dao.select_loading_detail()
     # 扣除操作
