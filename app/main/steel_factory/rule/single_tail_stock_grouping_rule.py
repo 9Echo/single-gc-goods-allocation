214af4e8a6169974fbe9fc38bc4568e904f996e2
刘伽椰 <18761691212@163.com>
2020-07-10 09:40:12 +0800

    增加方法
    增加货物分组和按品种获取重量下限方法

--- app/main/steel_factory/rule/single_tail_stock_grouping_rule.py
+++ app/main/steel_factory/rule/single_tail_stock_grouping_rule.py
@@ -3,6 +3,7 @@
 # Created: liujiaye  2020/07/10
 from typing import List
 from app.main.steel_factory.entity.stock import Stock
+from app.util.get_weight_limit import get_lower_limit
 
 
 def filter(stock_list: List[Stock]):
@@ -13,18 +14,18 @@ def filter(stock_list: List[Stock]):
     }
     for s in stock_list:
         # 获取重量下限
-        min_weight = 29000
+        min_weight = get_lower_limit(s.big_commodity_name)
 
         if s.actual_weight >= min_weight:
             stock_dic["huge"].append(s)
         else:
             # (可发件数+待产件数)*件重<重量下限
-            future_sum_weight=(s.actual_number+)*s.piece_weight
-            if future_sum_weight<=min_weight:
+            future_sum_weight = (s.actual_number + s.wait_production_number) * s.piece_weight
+            if future_sum_weight <= min_weight:
                 stock_dic["tail"].append(s)
             else:
-                if s.waint_fordel_weight>150000:
+                if s.waint_fordel_weight > 150000:
                     stock_dic["huge"].append(s)
                 else:
                     stock_dic["lock"].append(s)
-
+    return stock_dic
