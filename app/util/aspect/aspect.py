81826f78610aef8011f4029566e8a2ddc97a6cbf
邵鲁玉 <516508236@qq.com>
2020-07-14 09:33:12 +0800

    json

--- app/util/aspect/aspect.py
+++ app/util/aspect/aspect.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Description: 切面
 # Created: shaoluyu 2019/03/05
+import json
 from flask import request
 from datetime import datetime
 from app.main import blueprint
@@ -13,7 +14,7 @@ def before_request():
     current_app.logger.info('before_request')
     current_app.logger.info('method is ' + request.method)
     current_app.logger.info('url is ' + request.url)
-    current_app.logger.info('body is ' + str(request.json).replace('\'', '"'))
+    current_app.logger.info('body is ' + json.dumps(request.json, ensure_ascii=False))
     current_app.logger.info('ip is ' + request.remote_addr)
     current_app.logger.info('===========================end===============================')
 
@@ -22,7 +23,7 @@ def before_request():
 def after_request(response):
     current_app.logger.info('===========================start=============================')
     current_app.logger.info("after_request")
-    current_app.logger.info("body is " + str(response.json).replace("\'", '"'))
+    current_app.logger.info("body is " + json.dumps(response.json, ensure_ascii=False))
     current_app.logger.info("time is " + str(datetime.now())[:19])
     current_app.logger.info('===========================end===============================')
     return response
