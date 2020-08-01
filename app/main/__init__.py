001827b6fda17e46ee506a79a97b64b3ba0f0098
邵鲁玉 <516508236@qq.com>
2020-08-01 13:44:22 +0800

    1

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -6,8 +6,8 @@
 from flask import Blueprint
 from flask import jsonify
 from flask_restful import Api
-from app.main.routes.goods_allocation_route import GoodsAllocationRoute
 from app.main.routes.order_route import OrderRoute
+from app.main.routes.goods_allocation_route import GoodsAllocationRoute
 from app.main.routes.single_goods_allocation_route import SingleGoodsAllocationRoute
 
 blueprint = Blueprint('main', __name__)
