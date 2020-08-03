15a69d5b30de68ede35b062acab99b1d1b9d1f95
邵鲁玉 <516508236@qq.com>
2020-08-03 08:41:18 +0800

    根据历史匹配

--- app/main/pipe_factory/dao/match_dao.py
+++ app/main/pipe_factory/dao/match_dao.py
@@ -7,15 +7,27 @@ from app.util.base.base_dao import BaseDao
 class MatchDao(BaseDao):
     def select_match_data(self, order, max_delivery_items, min_delivery_items):
         sql = """
-        select
-        a.*
-        from
-        t_gc_history_match a,(SELECT city_name FROM `t_company_address` where company_id = '{}') b
-        where
-        a.city_name = b.city_name 
-        and
-        a.main_item_id in ({}) and (a.sub_item_id in ({}) or ifnull(a.sub_item_id,'') = '')
-        order by a.main_item_id,a.combine_count desc
+            select
+            distinct t1.main_item_id,t1.sub_item_id,t1.city_name,t1.main_quantity,t1.sub_quantity,t1.combine_count
+            from
+            t_gc_history_match t1,
+            (select
+            a.main_item_id,
+            a.sub_item_id,
+            a.city_name,
+            max(a.combine_count) as combine_count
+            from
+            t_gc_history_match a,(SELECT city_name FROM `t_company_address` where company_id = '{}') b
+            where
+            a.city_name = b.city_name 
+            and a.main_item_id in ({}) 
+            and (a.sub_item_id in ({}) or ifnull(a.sub_item_id,'') = '')
+            GROUP BY a.main_item_id,a.sub_item_id,a.city_name) t2 
+            where t1.main_item_id = t2.main_item_id 
+            and t1.sub_item_id = t2.sub_item_id
+            and t1.city_name = t2.city_name
+            and t1.combine_count = t2.combine_count	
+            order by t1.main_item_id,t1.combine_count desc
         """
         max_item_id_values = "'"
         max_item_id_values += "','".join([i.item_id for i in max_delivery_items])
